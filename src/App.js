c059a3f12584afca760f09abd4a1dccfea51c42b
昔日 <1017405960@qq.com>
2022-05-11 23:40:21 +0800

    项目基本配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from '@/pages/Login';
+import Layout from '@/pages/Layout';
+import {Button} from 'antd'
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    // 路由配置
+    <BrowserRouter>
+      <div className="App">
+        <Button type='primary'>hahah</Button>
+        <Routes>
+          {/* 创建路由path 和组件对应关系 */}
+          <Route path='/' element={<Layout/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom  from 'react-dom'
 import App from './App';
+// 导入antd样式文件 tips:如果引入的是 /antd.css 会报警告，以下是 issues 的解决办法
+import 'antd/dist/antd.min.css'
+
+// 引入index.scss文件
+import './index.scss'
 
 ReactDom.render(
   <React.StrictMode>
